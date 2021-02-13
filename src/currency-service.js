161380560a1be969a4449ca7195b8eb96495ab16
Woo Jin Kim <kimwoojin211@gmail.com>
2021-02-12 17:01:17 -0800

    fix formatting and delete extraneous comments.

--- src/currency-service.js
+++ src/currency-service.js
@@ -1,13 +1,12 @@
-
 export default class CurrencyService {
-  static async convert(currency1, currency2){
+  static async convert(currency1, currency2) {
     try {
       const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${currency1}/${currency2}`);
-      if(!response.ok) {
-          throw Error(response.status);
+      if (!response.ok) {
+        throw Error(response.status);
       }
       return response.json();
-    } catch(error){
+    } catch (error) {
       return error.message;
     }
   }
--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-/*wooj notes: */
-
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -9,41 +7,39 @@ import CurrencyService from './currency-service.js';
 let validCodes = [];
 loadCodes();
 
-function getElements(response){
-  if (response.result==="success"){
+function getElements(response) {
+  if (response.result === "success") {
     const convrate = response.conversion_rate;
     const base_code = response.base_code;
     const target_code = response.target_code;
     $("#conversion").text(`The conversion rate is 1 ${base_code} : ${convrate} ${target_code}`)
     $("#conversion").show();
     const amount = $("#amount").val();
-    if(amount==="")
-      {
-        $("#calculated").text(`Try entering a valid number in the box to see how much how much ${target_code} you would have.`);
-      }
-    else if(isNaN(parseInt(amount)))
-    {
+    console.log(parseInt(amount));
+    if (amount === "") {
+      $("#calculated").text(`Try entering a valid number in the box to see how much how much ${target_code} you would have.`);
+    }
+    else if (isNaN(parseInt(amount))) {
       $("#calculated").text(`Error. Invalid amount detected. Please enter a valid number.`);
     }
-    else{
-      const baseamount = new Intl.NumberFormat('en-US', { style: 'currency', currency: base_code }).format(amount);
-      const calculated = new Intl.NumberFormat('en-US', { style: 'currency', currency: target_code }).format(amount * convrate);
+    else {
+      const baseamount = new Intl.NumberFormat('en-US', { style: 'currency', currency: base_code }).format(parseInt(amount));
+      const calculated = new Intl.NumberFormat('en-US', { style: 'currency', currency: target_code }).format(parseInt(amount) * convrate);
       $("#calculated").text(`${baseamount} ${base_code} = ${calculated} ${target_code}`);
     }
     $("#calculated").show();
   }
-  else{
-    //Error! Error Code: unsupported code. Base Code is not a valid currency
+  else {
     let message = `Error! Error Code: `;
-    if(response.result === "error") 
+    if (response.result === "error")
       message += `${response["error-type"]}`;
     else
       message += `${response}.`;
     $("#conversion").text(message);
-    if (/^[a-z]{3}$/.test($("#base_code").val()))
-      $("#conversion").append("Base code is not a valid ISO 4217 code");
-    if (/^[a-z]{3}$/.test($("#target_code").val()))
-      $("#conversion").append("Target code is not a valid ISO 4217 code");
+    if (!(validCodes.includes($("#base_code").val())))
+      $("#conversion").append(`<br> ${$("#base_code").val()} is not a valid ISO 4217 code`);
+    if (!(validCodes.includes($("#target_code").val())))
+      $("#conversion").append(`<br> ${$("#target_code").val()} is not a valid ISO 4217 code`);
     $("#calculated").hide();
   }
 }
@@ -55,17 +51,16 @@ async function loadCodes() {
   });
 }
 
-async function makeApiCall(currency1,currency2)
-{
-  const response = await CurrencyService.convert(currency1,currency2);
+async function makeApiCall(currency1, currency2) {
+  const response = await CurrencyService.convert(currency1, currency2);
   getElements(response);
 }
 
-$("#convert").on("click",function(){
-  if(validCodes.length!==0)
+$("#convert").on("click", function () {
+  if (validCodes.length !== 0)
     makeApiCall($("#base_code").val(), $("#target_code").val());
 });
 
-$("input").on("submit",function(event){
+$("input").on("submit", function (event) {
   event.preventDefault();
 });
