4be5e614a0e5d1bd52b7976fbdb55b83c8a5c2d4
Woo Jin Kim <kimwoojin211@gmail.com>
2021-02-21 21:44:12 -0800

    remove global variable, update README and eslint config.

--- README.md
+++ README.md
@@ -21,18 +21,20 @@
 
 ## Setup/Installation Requirements
 
+* Visit the ExchangeRate-API site.
+* Create an account, 
+* Log-in, and click the "Get Free Key" button.
+* At this point, you'll be able to access a dashboard that includes an API key as well as your remaining API calls for the month. 
 * Clone this repository to your desktop
 * Navigate to the top level of the directory
-* Visit the ExchangeRate-API site. Input your email address and click the "Get Free Key" button.
-* You'll be prompted to create an account with your email, first name and a password. Agree to the terms of use and click "Get Started!"
-* At this point, you'll be able to access a dashboard that includes your API key as well as your remaining API calls for the month. Place this token in an .env file at the top level of your directory. 
-* Name your API key variable API_KEY in .env. Keep spelling and capitalization identical.
-* Include .env in .gitignore.
-* Confirm you are in the top level of the directory and enter `npm install` and `npm run start` into your terminal.
+* Create a new .env file in the top level of the project directory and open with your text editor.
+* Type `API_KEY={YOUR-API-KEY}` on line 1, replacing `{YOUR-API-KEY}` with the API Key obtained from the ExchangeRate-API site.
+* Include .env in your .gitignore.
+* Ensure Node and NPM are installed.
+* Enter `npm install` and `npm run start` into your terminal.
 
 ## Known Bugs
 
-* _Does not check the input of the optional base amount value very well. Code still runs even if numerals aren't used._
 * _No CSS done_
 
 ## License
--- src/main.js
+++ src/main.js
@@ -4,28 +4,32 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CurrencyService from './currency-service.js';
 
-let validCodes = [];
-loadCodes();
-
 function getElements(response) {
+  let codes = $("#dropdownBase>option");
+  let codes2 = [];
+  for(let element of codes){
+    codes2.push(element.value);
+  }
+  // is there a more concise way to do the above?
   if (response.result === "success") {
     const convrate = response.conversion_rate;
     const base_code = response.base_code;
     const target_code = response.target_code;
-    $("#conversion").text(`The conversion rate is 1 ${base_code} : ${convrate} ${target_code}`)
+    $("#conversion").text(`The conversion rate is 1 ${base_code} : ${convrate} ${target_code}`);
     $("#conversion").show();
-    const amount = $("#amount").val();
+    let amount = $("#amount").val();
     console.log(parseInt(amount));
     if (amount === "") {
-      $("#calculated").text(`Try entering a valid number in the box to see how much how much ${target_code} you would have.`);
+      $("#calculated").text(`Try entering a valid number in the box to see how much ${target_code} you would have.`);
     }
-    else if (isNaN(parseInt(amount))) {
+    else if (/[^\d.,]/.test(amount)) {
       $("#calculated").text(`Error. Invalid amount detected. Please enter a valid number.`);
     }
     else {
-      const baseamount = new Intl.NumberFormat('en-US', { style: 'currency', currency: base_code }).format(parseInt(amount));
-      const calculated = new Intl.NumberFormat('en-US', { style: 'currency', currency: target_code }).format(parseInt(amount) * convrate);
-      $("#calculated").text(`${baseamount} ${base_code} = ${calculated} ${target_code}`);
+      amount = amount.replace(/,/g,"");
+      const baseamount = new Intl.NumberFormat('en-US', { style: 'currency', currency: base_code }).format(parseFloat(amount));
+      const calculated = new Intl.NumberFormat('en-US', { style: 'currency', currency: target_code }).format(parseFloat(amount) * convrate);
+      $("#calculated").text(`${baseamount} = ${calculated}`);
     }
     $("#calculated").show();
   }
@@ -36,31 +40,43 @@ function getElements(response) {
     else
       message += `${response}.`;
     $("#conversion").text(message);
-    if (!(validCodes.includes($("#base_code").val())))
+    if (!(codes2.includes($("#base_code").val())))
       $("#conversion").append(`<br> ${$("#base_code").val()} is not a valid ISO 4217 code`);
-    if (!(validCodes.includes($("#target_code").val())))
+    if (!(codes2.includes($("#target_code").val())))
       $("#conversion").append(`<br> ${$("#target_code").val()} is not a valid ISO 4217 code`);
     $("#calculated").hide();
   }
 }
 
-async function loadCodes() {
-  const response = await CurrencyService.callForCodes();
-  Object.keys(response.conversion_rates).forEach(element => {
-    validCodes.push(element);
-  });
+
+async function makeApiCall(currency1, currency2) {  
+  const codeObject = await CurrencyService.callForCodes();
+  const codes = Object.keys(codeObject.conversion_rates);
+  const response = await CurrencyService.convert(currency1, currency2);
+  getElements(response,codes);
 }
 
-async function makeApiCall(currency1, currency2) {
-  const response = await CurrencyService.convert(currency1, currency2);
-  getElements(response);
+async function getCodes() {
+  const codeObject = await CurrencyService.callForCodes();
+  const codes = Object.keys(codeObject.conversion_rates);
+  dropDown(codes);
 }
+
+function dropDown(codes){
+  for (let i = 0; i < codes.length; i++) {
+    $("#dropdownBase")[0].innerHTML = $("#dropdownBase")[0].innerHTML + `<option value="${codes[i]}">`;
+    $("#dropdownTarget")[0].innerHTML = $("#dropdownTarget")[0].innerHTML + `<option value="${codes[i]}">`;
+  }
+}
+
+
+getCodes();
 
 $("#convert").on("click", function () {
-  if (validCodes.length !== 0)
-    makeApiCall($("#base_code").val(), $("#target_code").val());
+  makeApiCall($("#base_code").val(), $("#target_code").val());
 });
 
 $("input").on("submit", function (event) {
   event.preventDefault();
+
 });
